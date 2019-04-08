6b061746e22ddb5c49dd0e61e27b778120c32b07
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-04-08 16:08:58 -0600

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+import json
+
+setup(
+    name='MOTorNOT',
+    version='0.1',
+    description='Magneto-optical trap simulation package',
+    author='Robert Fasano',
+    author_email='robert.j.fasano@colorado.edu',
+    packages=find_packages(exclude=['docs']),
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=['ipython', 'matplotlib', 'pandas', 'scipy', 'jupyter']
+)
